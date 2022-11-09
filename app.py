d160a9ce32ec67ceb61c13f7d7d9f37f6bb3bd34
Federico Ramirez <federicoeramirez@gmail.com>
2022-11-08 22:46:00 -0300

    Testing front button

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import spotipy
 
 '''
 # JARS test
@@ -14,11 +15,11 @@ Search for your favorite song and get a list of recommended songs!
 # - amount of recommended songs
 
 song = st.text_input('Insert song and artist names:', placeholder = 'song name + artist name')
-amount = st.number_input('Insert amount of songs:', min_value=1, max_value=50, value=15)
+amount = st.number_input('Insert amount of songs:', min_value=5, max_value=50, value=20)
 #playlist = st.checkbox('Create new playlist:', value=False)
 
 url = 'https://jars-test-3l6llep5va-wl.a.run.app/recommendation'
-
+#url = 'https://127.0.0.1:8000/recommendation'
 # 2. Let's build a dictionary containing the parameters for our API...
 
 params = {
@@ -28,13 +29,48 @@ params = {
 }
 
 # 3. Let's call our API using the `requests` package...
+if st.button('Get Recommendation'):
+    response = requests.get(url, params=params)
 
-response = requests.get(url, params=params)
+    # 4. Let's retrieve the prediction from the **JSON** returned by the API...
 
-# 4. Let's retrieve the prediction from the **JSON** returned by the API...
+    prediction = response.json()
 
-prediction = response.json()
+    ## Finally, we can display the prediction to the user
 
-## Finally, we can display the prediction to the user
+    st.write(prediction)
 
-st.write(prediction)
+if st.button('Make Playlist'):
+
+    username = None
+    client_id = 'b8c39473bf4a4b7ea9deeadad8ecd9ae'
+    client_secret = 'c3a78c3d7ba54691b07e9198cfe8783e'
+
+    # With authentication
+    sp = spotipy.Spotify(
+        auth_manager= spotipy.SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri="http://127.0.0.1:9090",
+            scope="playlist-modify-private",
+            #scope="user-library-read"
+            )
+        )
+
+    user_id = sp.me()['id']
+
+    playlist = sp.user_playlist_create(user_id,
+                                       'test',
+                                       public=False,
+                                       collaborative=False,
+                                       description='Testing')
+
+    playlist_id = playlist['id']
+
+    top_tracks_list = ['3Bdqlr7jQLNhITAgcBGQBG',
+                       '7sLpSWxQazJzDVG6YGzlVs']
+
+    sp.user_playlist_add_tracks(user_id,
+                            playlist_id,
+                            top_tracks_list,
+                            position=None)
